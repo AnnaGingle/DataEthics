5b4c3dab83edc8afcf3449ba06539633b158b186
Anna Gingle <48488699+AnnaGingle@users.noreply.github.com>
2019-11-25 12:47:26 -0600

    Add files via upload
    Desktop only because this is for a class presentation.

---
+++ script.js
@@ -0,0 +1,57 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navlinks = document.querySelectorAll(".nav-links li");
+
+
+    burger.addEventListener("click", () => {
+        //toggle nav
+        nav.classList.toggle("nav-active");
+
+        //animate links
+        navlinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forward ${index / 7}s`;
+            }
+        });
+        //burger animation
+        burger.classList.toggle("toggle");
+    });
+
+}
+
+
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
+
+
+
+
+navSlide();
